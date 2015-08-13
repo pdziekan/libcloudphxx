07bc1f291971d490bf40cbfd0ce576118b33c724
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2015-08-13 17:08:37 +0200

    update settings

--- tests/paper_GMD_2015/src/icmw8_case1.hpp
+++ tests/paper_GMD_2015/src/icmw8_case1.hpp
@@ -50,8 +50,8 @@ namespace icmw8_case1
     sdev_rd1 = real_t(1.4),
     sdev_rd2 = real_t(1.6);
   const quantity<power_typeof_helper<si::length, static_rational<-3>>::type, real_t>
-    n1_stp = real_t(60e6 ) / si::cubic_metres,
-    n2_stp = real_t(40e6 ) / si::cubic_metres;
+    n1_stp = real_t(60e6) / si::cubic_metres,
+    n2_stp = real_t(40e6) / si::cubic_metres;
 
   const quantity<power_typeof_helper<si::length, static_rational<-3>>::type, real_t>
     n1_stp_src = real_t(60e6 / 2000. / 30.) / si::cubic_metres, 
