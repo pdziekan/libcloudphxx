3cbef6bbe3721906d11086f6835f4926588c76bd
Piotr Dziekan <pdziekan@eyrie.prac.igf>
2015-11-17 16:15:56 -0700

    icicle: turb 400 20k no GCCN cond + coal

--- tests/paper_GMD_2015/src/kin_cloud_2d_lgrngn.hpp
+++ tests/paper_GMD_2015/src/kin_cloud_2d_lgrngn.hpp
@@ -121,7 +121,7 @@ class kin_cloud_2d_lgrngn : public kin_cloud_2d_common<ct_params_t>
   bool get_rain() { return true;/*params.cloudph_opts.coal && params.cloudph_opts.sedi; */}
   void set_rain(bool val) 
   { 
-    params.cloudph_opts.coal = params.cloudph_opts.sedi = val; 
+    params.cloudph_opts.coal /*= params.cloudph_opts.sedi*/ = val; 
     params.cloudph_opts.RH_max = val ? 44 : 1.01; // 1% limit during spinup // TODO: specify it somewhere else, dup in blk_2m
   };
 
