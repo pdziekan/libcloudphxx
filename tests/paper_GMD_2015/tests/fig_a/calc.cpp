948a939813d36592d33ba05bd2e79a4bb5dfd3e7
Piotr Dziekan <plgpdziekan@zeus.cyfronet.pl>
2015-12-04 19:28:11 +0100

    out 1st rd mom of all

--- tests/paper_GMD_2015/tests/fig_a/calc.cpp
+++ tests/paper_GMD_2015/tests/fig_a/calc.cpp
@@ -44,7 +44,7 @@ int main(int ac, char** av)
         + bins_wet_str + // aerosol spectrum (wet)
       "\""
       " --out_dry=\""
-        "1e-9:1|2,3;" // effective dry radius
+        "1e-9:1|1,2,3;" // effective dry radius
         + bins_dry_str + // aerosol spectrum (dry)
       "\""
   });
