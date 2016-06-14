be0e5bf0d56407df21f7ec5a1ee6bd44aedaee1f
pdziekan <pdziekanp@gmail.com>
2016-06-14 12:41:04 +0200

    dycoms2d set z_rlx 25

--- tests/DYCOMS2D/src/setup.hpp
+++ tests/DYCOMS2D/src/setup.hpp
@@ -161,7 +161,7 @@ namespace setup
 
   //th, rv and surface fluxes relaxation time and height
   const quantity<si::time, real_t> tau_rlx = 300 * si::seconds;
-  const quantity<si::length, real_t> z_rlx = 200 * si::metres;
+  const quantity<si::length, real_t> z_rlx = 25 * si::metres;
 
   // function expecting a libmpdata solver parameters struct as argument
   template <class T>
