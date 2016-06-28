91ba9dc56a9aae5b380c9164ef47418edf1b58f7
pdziekan <pdziekanp@gmail.com>
2016-06-28 14:10:16 +0200

    Dycoms3d: catch up 3d_common.cpp with 2d

--- tests/DYCOMS3D/src/kin_cloud_3d_common.hpp
+++ tests/DYCOMS3D/src/kin_cloud_3d_common.hpp
@@ -20,7 +20,7 @@ class kin_cloud_3d_common : public
 
   // relaxation stuff
   bool relax_th_rv;
-  blitz::Array<typename ct_params_t::real_t, 3> th_eq, rv_eq;
+  blitz::Array<typename ct_params_t::real_t, 3> th_eq, rv_eq, th_ref, rhod;
   
   // spinup stuff
   virtual bool get_rain() = 0;
@@ -37,7 +37,10 @@ class kin_cloud_3d_common : public
     if (spinup > 0)
     {
       blitz::thirdIndex k;
+      // initially the enviromental and reference profiles are the initial profiles
+      rhod = setup::rhod_fctr()(k * dz);
       th_eq = setup::th_dry_fctr()(k * dz);
+      th_ref = th_eq;
       rv_eq = setup::r_t()(k * dz);
 
       set_rain(false);
@@ -61,11 +64,51 @@ class kin_cloud_3d_common : public
       // they will be the relaxation goals
       // TODO: when calculating mean, do not include first or last point (which is the same in cyclic boundaries);
       //       right now it is accounted for twice, but the concurrency-aware sum cannot exclude single point
+/*
       for (int k = this->k.first(); k <= this->k.last(); ++k)
       {  
         th_eq(this->i, this->j, k) = this->mem->sum(this->state(ix::th), this->i, this->j, rng_t(k, k), false)  /  (this->mem->grid_size[0].length() * this->mem->grid_size[1].length());
         rv_eq(this->i, this->j, k) = this->mem->sum(this->state(ix::rv), this->i, this->j, rng_t(k, k), false)  /  (this->mem->grid_size[0].length() * this->mem->grid_size[1].length());
       }
+*/
+    // calculate reference theta and rhod
+    // like in Wojtek's code
+    {
+/*
+      blitz::secondIndex k;
+      int nz = this->mem->grid_size[1].length();
+      // calculate average stability
+      blitz::Range notopbot(1, nz-2);
+      blitz::Array<double, 1> st(nz);
+      st=0;
+      st(notopbot) = (th_eq(0, notopbot+1) - th_eq(0, notopbot-1)) / th_eq(0, notopbot);
+      double st_avg = blitz::sum(st) / (nz-2) / (2.*dz);
+      // reference theta
+      th_ref = th_eq(0,0) * exp(st_avg * k * dz);
+      // virtual temp at surface
+      using libcloudphxx::common::moist_air::R_d_over_c_pd;
+      using libcloudphxx::common::moist_air::c_pd;
+      using libcloudphxx::common::moist_air::R_d;
+      using libcloudphxx::common::theta_std::p_1000;
+
+      double T_surf = th_eq(0, 0) *  pow(setup::p_0 / p_1000<double>(),  R_d_over_c_pd<double>());
+      double T_virt_surf = T_surf * (1. + 0.608 * rv_eq(0, 0));
+      double rho_surf = (setup::p_0 / si::pascals) / T_virt_surf / 287. ; // TODO: R_d instead of 287
+      double cs = 9.81 / (c_pd<double>() / si::joules * si::kilograms * si::kelvins) / st_avg / T_surf;
+      // rhod profile
+      rhod = rho_surf * exp(- st_avg * k * dz) * pow(
+               1. - cs * (1 - exp(- st_avg * k * dz)), (1. / R_d_over_c_pd<double>()) - 1);
+*/
+//      g_factor() = rhod;
+
+/*
+      std::cout << "post-spinup profiles:" << std::endl;
+      std::cout << "rhod: " << rhod;
+      std::cout << "th_ref: " << th_ref;
+      std::cout << "th_eq: " << th_eq;
+      std::cout << "rv_eq: " << rv_eq;
+*/
+    }
     }
 
     parent_t::hook_ante_step(); 
@@ -131,6 +174,12 @@ class kin_cloud_3d_common : public
     spinup(p.spinup),
     relax_th_rv(p.relax_th_rv),
     th_eq(this->mem->grid_size[0].length(),
+          this->mem->grid_size[1].length(),
+          this->mem->grid_size[2].length()),
+    th_ref(this->mem->grid_size[0].length(),
+          this->mem->grid_size[1].length(),
+          this->mem->grid_size[2].length()),
+    th_rhod(this->mem->grid_size[0].length(),
           this->mem->grid_size[1].length(),
           this->mem->grid_size[2].length()),
     rv_eq(this->mem->grid_size[0].length(),
