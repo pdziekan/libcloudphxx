fc590efe1e3f600b0ce9e6e3d0e75d6519f5b686
pdziekan <pdziekanp@gmail.com>
2016-06-28 14:03:41 +0200

    Dycoms3d: catch up forcings with 2d

--- tests/DYCOMS3D/src/forcings.hpp
+++ tests/DYCOMS3D/src/forcings.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 template <class ct_params_t>
-void kin_cloud_3d_lgrngn<ct_params_t>::buoyancy(const blitz::Array<real_t, 3> &th)
+void kin_cloud_3d_lgrngn<ct_params_t>::buoyancy(const blitz::Array<real_t, 3> &th, const blitz::Array<real_t, 3> &rv)
 {
   const auto &ijk = this->ijk;
   const auto &i = this->i;
@@ -12,7 +12,7 @@ void kin_cloud_3d_lgrngn<ct_params_t>::buoyancy(const blitz::Array<real_t, 3> &t
 
 //  namespace moist_air = libcloudphxx::common::moist_air;
 //  const real_t eps = moist_air::R_v<real_t>() / moist_air::R_d<real_t>() - 1.;
-  tmp1(ijk) = g * ((th(ijk) - th_init(ijk)) / th_init(0, 0, 0));// + eps * (rv - rv_init(ijk)));
+  tmp1(ijk) = g * ((th(ijk) - th_eq(ijk)) / th_ref(ijk));// + eps * (rv - rv_init(ijk)));
 
   this->xchng_sclr(tmp1, i, j, k); 
   F(i, j, k) = 0.25 * (tmp1(i, j, k + 1) + 2 * tmp1(i, j, k) + tmp1(i, j, k - 1));
@@ -43,37 +43,59 @@ void kin_cloud_3d_lgrngn<ct_params_t>::radiation(const blitz::Array<real_t, 3> &
     {
       for(int z = 0 ; z < nz; ++z)
       {
-        real_t sum = r_l(x, y, z) / 2.;
-        if(z < nz-1) sum += blitz::sum(r_l(x, y, blitz::Range(z+1, nz-1)));
-        F(x, y, z) += setup::F_0 * exp(- (nz - z) * this->dk * sum); 
-
-        sum = r_l(x, y, z) / 2.;
-        if(z > 0) sum += blitz::sum(r_l(x, y, blitz::Range(0, z-1)));
-        F(x, y, z) += setup::F_1 * exp(- ((z) - 0) * this->dk * sum);
-
-        if(z >= k_i(x, y) )
+        double sum = blitz::sum(r_l(x, y, blitz::Range(z, nz-1)));
+        if(z==0)
+          F(x, y, z) += setup::F_0 * exp(- (nz - z - 1) * this->dk * sum); 
+        else
+          F(x, y, z) += setup::F_0 * exp(- (nz - z - 0.5) * this->dk * sum); 
+   
+        if(z > 0)
         {
-          real_t z_i = (-0.5 + k_i(x,y)) * this->dk;
-          real_t z_d = z * this->dk - z_i;
+          sum = blitz::sum(r_l(x, y, blitz::Range(0, z-1)));
+          F(x, y, z) += setup::F_1 * exp(- (z - 0.5) * this->dk * sum);
+        }
+  
+        if(z > k_i(x, y) )
+        {
+          real_t z_i = (k_i(x, y) - .5) * this->dk; // bottom of the first cell above inversion, z=0 at k=0.5
+          real_t z_d = (z - 0.5) * this->dk - z_i;
           F(x, y, z) += setup::c_p * setup::rho_i * setup::D * (0.25 * pow(z_d, 4./3) + z_i * pow(z_d, 1./3)); 
         }
       }
     }
   }
+  // smoothing
+  tmp1(ijk)=F(ijk);
+  this->xchng_sclr(tmp1, i, j, k); 
+  F(i, j) = 0.25 * (tmp1(i, j, k + 1) + 2 * tmp1(i, j, k) + tmp1(i, j, k - 1));
 }
 
 template <class ct_params_t>
 void kin_cloud_3d_lgrngn<ct_params_t>::surf_sens()
 {
   const auto &ijk = this->ijk;
-  F(ijk) = setup::F_sens * hgt_fctr(ijk);
+  F(ijk) =  setup::F_sens * hgt_fctr_sclr(ijk); 
+// smoothing
+  const auto &i = this->i;
+  const auto &j = this->j;
+  const auto &k = this->k;
+  tmp1(ijk)=F(ijk);
+  this->xchng_sclr(tmp1, i, j, k); 
+  F(i, j, k) = 0.25 * (tmp1(i, j, k + 1) + 2 * tmp1(i, j, k) + tmp1(i, j, k - 1));
 }
 
 template <class ct_params_t>
 void kin_cloud_3d_lgrngn<ct_params_t>::surf_latent()
 {
   const auto &ijk = this->ijk;
-  F(ijk) =  setup::F_lat * hgt_fctr(ijk); 
+  F(ijk) =  setup::F_lat * hgt_fctr_sclr(ijk); 
+// smoothing
+  const auto &i = this->i;
+  const auto &j = this->j;
+  const auto &k = this->k;
+  tmp1(ijk)=F(ijk);
+  this->xchng_sclr(tmp1, i, j, k); 
+  F(i, j, k) = 0.25 * (tmp1(i, j, k + 1) + 2 * tmp1(i, j, k) + tmp1(i, j, k - 1));
 }
 
 template <class ct_params_t>
@@ -85,5 +107,9 @@ void kin_cloud_3d_lgrngn<ct_params_t>::subsidence(const int &type) // large-scal
   const auto &k = this->k;
   tmp1(ijk) = this->state(type)(ijk);
   this->xchng_sclr(tmp1, i, j, k);
-  F(i, j, k) = - w_LS(i, j, k) * (tmp1(i, j, k + 1) - tmp1(i, j, k - 1)) / (2. * this->dk); // use built-in blitz stencil?
+  F(i, j, k) = - w_LS(i, j, k) * (tmp1(i, j, k + 1) - tmp1(i, j, k - 1)) / (2. * this->dk);
+  // smoothing
+  tmp1(ijk)=F(ijk);
+  this->xchng_sclr(tmp1, i, j, k); 
+  F(i, j, k) = 0.25 * (tmp1(i, j, k + 1) + 2 * tmp1(i, j, k) + tmp1(i, j, k - 1));
 }
