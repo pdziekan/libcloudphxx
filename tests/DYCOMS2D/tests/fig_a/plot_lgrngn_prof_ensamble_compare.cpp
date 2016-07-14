309e716fb1e23caacb1b1ec7dab616d88d3a9523
pdziekan <pdziekanp@gmail.com>
2016-07-14 11:17:05 +0200

    dycoms2d: series compare plot

--- tests/DYCOMS2D/tests/fig_a/plot_lgrngn_prof_ensamble_compare.cpp
+++ tests/DYCOMS2D/tests/fig_a/plot_lgrngn_prof_ensamble_compare.cpp
@@ -24,32 +24,36 @@ int main(int argc, char* argv[])
   for (auto &plt : profs) 
   {
     Gnuplot gp;
-    std::string file = argv[1] + std::string("/") + plt + std::string("_profiles_compare.svg");
+    std::string file = argv[1] + std::string("/profiles_compare") + plt + std::string(".svg");
     std::cout << "out file: " << file << std::endl;
     init_prof(gp, file, 1, 1);
+    gp << "set ylabel 'height / inversion height'\n";
 
     if (plt == "rliq")
-      gp << "set title 'liquid water r [g/kg] averaged over 2h-6h, w/o rw<0.5um'\n";
+      gp << "set xlabel 'liquid water mixing ratio [g/kg]'\n";
     else if (plt == "00rtot")
-      gp << "set title 'total water r [g/kg] averaged over 2h-6h, w/o rw<0.5um'\n";
+      gp << "set xlabel 'total water mixing ratio [g/kg]'\n";
     else if (plt == "N_c")
-      gp << "set title 'cloud droplets ( 0.5um < r < 25um) concentration [1/cm^3]'\n";
+      gp << "set xlabel 'cloud droplets concentration [1/cm^3]'\n";
     else if (plt == "thl")
-      gp << "set title 'liquid potential temp [K]'\n";
+      gp << "set xlabel 'liquid potential temp [K]'\n";
     else if (plt == "clfrac")
-      gp << "set title 'cloud fraction'\n";
+      gp << "set xlabel 'cloud fraction'\n";
     else if (plt == "prflux")
-      gp << "set title 'precipitation flux [W/m^2]'\n";
+      gp << "set xlabel 'precipitation flux [W/m^2]'\n";
     else if (plt == "wvar")
-      gp << "set title 'variance of w [m^2 / s^2]'\n";
+      gp << "set xlabel 'variance of w [m^2 / s^2]'\n";
     else if (plt == "w3rd")
-      gp << "set title '3rd mom of w [m^3 / s^3]'\n";
+      gp << "set xlabel '3rd mom of w [m^3 / s^3]'\n";
     else if (plt == "act_rd")
-      gp << "set title 'activated droplets mean dry radius [um]'\n";
+    {
+      gp << "set xlabel 'activated droplets mean dry radius [um]'\n";
+      gp << "set xrange [0:0.15]\n";
+    }
     else if (plt == "gccn_rw")
-      gp << "set title 'GCCN-based droplets mean wet radius [um]'\n";
+      gp << "set xlabel 'GCCN-based droplets mean wet radius [um]'\n";
     else if (plt == "gccn_rw_down")
-      gp << "set title 'GCCN-based droplets mean wet radius [um] in downdraught regions'\n";
+      gp << "set xlabel 'GCCN-based droplets mean wet radius [um] in downdraught regions'\n";
 
     gp << "plot '-' with line t '" << argv[2] << "'";
     for(int j=3; j<argc; j+=2)
@@ -77,7 +81,7 @@ int main(int argc, char* argv[])
         iprof_file >> snap;
       double z_i;
       iprof_file >> z_i;
-      res_pos = (fi-0.5) * 5. / z_i; // hardcoded dz = 5m !!
+      res_pos = (fi-1.5) * 5. / z_i; // hardcoded dz = 5m !!
       std::cout << i << " " << plt << " res: " << res;
       std::cout << i << " " << plt << " res_pos: " << res_pos;
       gp.send1d(boost::make_tuple(res, res_pos));
