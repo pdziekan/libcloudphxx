8bbb79910fecc3a89f5350570abec4ded671a1b5
pdziekan <pdziekanp@gmail.com>
2016-08-17 11:59:05 +0200

    dycoms2d: remove obsolete th updates

--- tests/DYCOMS2D/src/kin_cloud_2d_lgrngn.hpp
+++ tests/DYCOMS2D/src/kin_cloud_2d_lgrngn.hpp
@@ -331,8 +331,6 @@ class kin_cloud_2d_lgrngn : public kin_cloud_2d_common<ct_params_t>
   {
     parent_t::hook_post_step(); // includes output
 
-    // store -rv_old in tmp1
-    tmp1(this->ijk) = - this->mem->advectee(ix::rv)(this->ijk);
     this->mem->barrier();
     if (this->rank == 0) 
     {
@@ -437,22 +435,6 @@ class kin_cloud_2d_lgrngn : public kin_cloud_2d_common<ct_params_t>
       }
     }
     this->mem->barrier();
-    // get delta_rv in tmp1
-    tmp1(this->ijk) += this->mem->advectee(ix::rv)(this->ijk);
-    // update theta accordin to drv
-    using libcloudphxx::common::moist_air::c_pd;
-    using libcloudphxx::common::const_cp::l_tri;
-    // TODO: reenable it after making sure its right
-    /*
-    for(int x = this->i.first() ; x <= this->i.last(); ++x)
-    {
-      for(int z = this->j.first() ; z <= this->j.last(); ++z)
-      {
-        this->mem->advectee(ix::th)(x,z) += - tmp1(x,z) * (l_tri<setup::real_t>() / c_pd<setup::real_t>() / si::kelvin) *
-          this->mem->advectee(ix::th)(x,z) / (libcloudphxx::common::theta_dry::T<real_t>(this->state(ix::th)(x, z) * si::kelvins, this->rhod(x, z) * si::kilograms / si::metres  / si::metres / si::metres) / si::kelvins); 
-      }
-    }
-*/
   }
 
   public:
